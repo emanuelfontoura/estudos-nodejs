e43aed060120c2fe2886783899d355df7d3ee67f
emanuelfontoura <emanuelfontoura03@gmail.com>
2024-04-10 12:49:18 -0300

    estudos

---
+++ 3_COREMODULES/12_path/index.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+const customPath = 'C:/Documentos/Relatórios/Emanuel/relatorio1.pdf'
+
+console.log(path.dirname(customPath))
+console.log(path.basename(customPath))
+console.log(path.extname(customPath))
