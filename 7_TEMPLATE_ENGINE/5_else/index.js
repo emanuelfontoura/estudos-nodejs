1c0cb76a8fb115156dca314cb9f4fc60001501a9
emanuelfontoura <emanuelfontoura03@gmail.com>
2024-05-07 22:03:24 -0300

    estudos

---
+++ 7_TEMPLATE_ENGINE/5_else/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/sobre', ((req, res) => {
+    res.render('sobre')
+}))
+
+app.get('/dashboard', ((req, res) => {
+    const auth = true
+    res.render('dashboard', {auth})
+}))
+
+app.get('/', ((req, res) => {
+    const user = {
+        name : 'Emanuel',
+        surname : 'Fontoura'
+    }
+    const palavra = 'Teste'
+    res.render('home', {user, palavra})
+}))
+
+app.listen(3000)
---
+++ 7_TEMPLATE_ENGINE/6_estrutura_repetição/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/sobre', ((req, res) => {
+    res.render('sobre')
+}))
+
+app.get('/dashboard', ((req, res) => {
+    const auth = true
+    const approvedUsers = ['Emanuel', 'Matheus', 'João']
+    res.render('dashboard', {auth, approvedUsers})
+}))
+
+app.get('/', ((req, res) => {
+    const user = {
+        name : 'Emanuel',
+        surname : 'Fontoura'
+    }
+    const palavra = 'Teste'
+    res.render('home', {user, palavra})
+}))
+
+app.listen(3000)
