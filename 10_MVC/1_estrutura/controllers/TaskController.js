e9741845e7866be4dcac3212d09d6e53bbfc6acf
emanuelfontoura <emanuelfontoura03@gmail.com>
2024-06-04 22:00:43 -0300

    estudos

--- 10_MVC/1_estrutura/controllers/TaskController.js
+++ 10_MVC/1_estrutura/controllers/TaskController.js
@@ -7,12 +7,36 @@ module.exports = class TaskController {
     }
 
     static async createTaskSave(req, res){
-        const {name, description} = req.body
-        await Task.create({name, description, done: false})
-        res.redirect('/')
+        const {title, description} = req.body
+        await Task.create({title, description})
+        res.redirect('/tasks')
     }
 
-    static showTasks(req, res){
-        res.render('tasks/show')
+    static async showTasks(req, res){
+        const tasks = await Task.findAll({raw:true})
+        res.render('tasks/show', {tasks})
+    }
+
+    static async showTask(req, res){
+        const task = await Task.findByPk(req.params.id, {raw:true})
+        res.render('tasks/showOneTask', {task})
+    }
+
+    static async removeTask(req, res){
+        const id = req.body.id
+        await Task.destroy({where: {id}})
+        res.redirect('/tasks')
+    }
+
+    static async editTask(req, res){
+        const id = req.params.id
+        const task = await Task.findByPk(id, {raw:true})
+        res.render('tasks/edit', {task})
+    }
+
+    static async editTaskSave(req, res){
+        const {title, description, id} = req.body
+        await Task.update({title, description}, {where: {id}})
+        res.redirect('/tasks')
     }
 }
--- 10_MVC/1_estrutura/routes/taskRoutes.js
+++ 10_MVC/1_estrutura/routes/taskRoutes.js
@@ -5,10 +5,18 @@ const TaskController = require('../controllers/TaskController.js')
 router.use(express.urlencoded({extended:true}))
 router.use(express.json())
 
+router.post('/editSave', TaskController.editTaskSave)
+
+router.get('/edit/:id', TaskController.editTask)
+
+router.post('/remove', TaskController.removeTask)
+
 router.get('/add', TaskController.createTask)
 
 router.post('/add', TaskController.createTaskSave)
 
 router.get('/', TaskController.showTasks)
+
+router.get('/:id', TaskController.showTask)
 
 module.exports = router
