71233c41b5c055e327d882d132008d53f091639e
Emanuel Victor Fontoura Cantalejo <emanuelfontoura03@gmail.com>
2024-06-25 22:28:44 -0300

    estudos

--- 11_TOUGHTS/controllers/AuthController.js
+++ 11_TOUGHTS/controllers/AuthController.js
@@ -11,11 +11,49 @@ module.exports = class AuthController{
     }
 
     static async registerPost(req, res){
-        const {name, email, password, confirmpassword} = req.body
-        if(password != confirmpassword){
+        const {username, email, password, confirmPassword} = req.body
+
+        // checando se as duas senhas são iguais
+        if(password != confirmPassword){
             req.flash('message', 'As senhas não conferem. Tente novamente!')
             res.render('auth/register')
             return
+        }
+
+        // checando se o usuário já existe
+        const checkIfUserExists = await User.findOne({where: {email:email}})
+
+        if(checkIfUserExists){
+            req.flash('message', 'Este email já está em uso. Tente outro!')
+            res.render('auth/register')
+            return
+        }
+
+        // aplicando salt e hash na senha
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name: username,
+            email,
+            password: hashedPassword,
+        }
+
+        try{
+            const createdUser = await User.create(user)
+
+            // iniciando session
+            req.session.userId = createdUser.id
+
+            req.flash('message', 'Usuário criado com sucesso!')
+
+            // salvando a session e depois redirecionando
+            req.session.save(() => {
+                res.redirect('/')
+            })
+        }catch(err){
+            req.flash('message', 'Erro ao criar usuário. Tente novamente!')
+            res.render('auth/register')
         }
     }
 }
