486f4bc804643758742bf7bcf7ed9f0aae3ed459
Emanuel Victor Fontoura Cantalejo <emanuelfontoura03@gmail.com>
2024-07-09 22:04:47 -0300

    estudos e prática

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -6,11 +6,42 @@ module.exports = class ToughtController{
         res.render('toughts/home')
     }
 
-    static dashboard(req, res){
-        res.render('toughts/dashboard')
+    static async dashboard(req, res){
+        const userId = req.session.userId
+
+        const user = await User.findOne({
+            where: {id: userId}, 
+            include : Tought,
+            plain: true
+        })
+
+        const toughts = user.Toughts.map((result) => result.dataValues)
+
+        if(!user){
+            res.redirect('/login')
+        }
+
+        res.render('toughts/dashboard', {toughts})
     }
 
     static createTought(req, res){
         res.render('toughts/create')
+    }
+
+    static async createToughtSave(req, res){
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userId
+        }
+
+        try{
+            await Tought.create(tought)
+            req.flash('message', 'Pensamento criado com sucesso!')
+            req.session.save(() => {
+                res.redirect('/dashboard')
+            })
+        }catch(error){
+            req.flash('message', 'Erro ao criar pensamento! ' + error)
+        }
     }
 }
